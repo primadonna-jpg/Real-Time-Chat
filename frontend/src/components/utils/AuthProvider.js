34945f398cc1224c98662bba224ab24c5e706a28
Bartek Nowak <bartek.spok@gmail.com>
2024-10-19 18:36:03 +0200

    Changed the way of creating chat with other users. Added option to create chat with more than one user

--- frontend/src/components/utils/AuthProvider.js
+++ frontend/src/components/utils/AuthProvider.js
@@ -5,10 +5,10 @@ import { jwtDecode } from 'jwt-decode';
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  //const baseURL = "http://127.0.0.1:8000";
-  const baseURL = "http://192.168.100.55:8000";
+  const baseURL = "http://127.0.0.1:8000";
+  //const baseURL = "http://192.168.100.55:8000";
   const [token, setToken] = useState(null);
-  const [currentUser, setUser] =useState(null);
+  const [currentUserUsername, setUserUsername] =useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -33,7 +33,7 @@ const AuthProvider = ({ children }) => {
 
   const decodeUsername = (token) => {
     const decodedToken = jwtDecode(token);
-    setUser( decodedToken.username || 'Unknown');
+    setUserUsername( decodedToken.username || 'Unknown');
      
   };
 
@@ -44,7 +44,7 @@ const AuthProvider = ({ children }) => {
   }
 
   return (
-    <AuthContext.Provider value={{ token,currentUser,baseURL, setNewToken, logout }}>
+    <AuthContext.Provider value={{ token,currentUserUsername,baseURL, setNewToken, logout }}>
       {children}
     </AuthContext.Provider>
   );
